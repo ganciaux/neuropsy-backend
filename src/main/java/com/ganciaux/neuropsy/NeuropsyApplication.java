5e52ad4cbca37d838a5a3048c549dd00f12855c1
ganciaux <ghislain.anciaux@internet.lu>
2023-12-20 14:28:56 +0100

    Init project

---
+++ src/main/java/com/ganciaux/neuropsy/NeuropsyApplication.java
@@ -0,0 +1,13 @@
+package com.ganciaux.neuropsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuropsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuropsyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganciaux/neuropsy/NeuropsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganciaux.neuropsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuropsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
